c4d8ddf0ae56970175e38c1b4cb485b2022dbc5d
Audel Diaz <audel.diaz@outlook.com>
2024-03-29 03:08:31 +0000

    add some code and tests

--- src/main.py
+++ src/main.py
@@ -1,8 +1,38 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from typing import Optional
 
-app = FastAPI()
+app = FastAPI()  # pragma: no mutate
 
-
-@app.get("/")
+@app.get("/", response_model=dict)
 async def root():
+    """
+    Root endpoint
+    """
     return {"message": "Hello"}
+
+@app.get("/sum", response_model=int)
+async def get_sum(a: int, b: int):
+    """
+    Return the sum of two integers
+    """
+    return a + b
+
+@app.get("/division", response_model=Optional[float])
+async def get_division(dividend: float, divisor: float) -> float:
+    """
+    Calculates the division of two numbers.
+
+    Args:
+        dividend (float): The number to divide.
+        divisor (float): The number to divide by.
+
+    Returns:
+        float: The result of the division.
+
+    Raises:
+        ValueError: If the divisor is zero.
+    """
+    if divisor == 0:
+        raise HTTPException(status_code=400, detail="Cannot divide by zero")
+    else:
+        return dividend / divisor
--- src/tests/main_test.py
+++ src/tests/main_test.py
@@ -1,3 +1,4 @@
+from random import random
 from fastapi.testclient import TestClient
 from main import app
 
@@ -8,3 +9,24 @@ def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "Hello"}
+
+
+def test_get_sum():
+    response = client.get("/sum", params={'a':2, 'b':4})
+    assert response.status_code == 200
+    assert response.json() == 6
+    
+    
+def test_get_division_ok():
+    dividend = float(random())
+    divisor = float(random())
+    response = client.get("/division", params={'dividend':dividend, 'divisor':divisor})
+    assert response.status_code == 200
+    assert response.json() == dividend/divisor
+    
+    
+def test_get_division_by_zero():
+    dividend = float(random())
+    response = client.get("/division", params={'dividend':dividend, 'divisor':0})
+    assert response.status_code == 400
+    assert response.json() == {'detail':'Cannot divide by zero'}  
